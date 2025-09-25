069a89944344717dee85d7b5283ded53ce1ab70c
SandroSchmidt <62615642+SandroSchmidt@users.noreply.github.com>
2025-09-26 00:49:41 +0200

    Add files via upload

---
+++ datav2.js
@@ -0,0 +1,30 @@
+
+waypoints =[{    
+  name:"Drink Station 1", coordinates:[144.9763960,-37.8391359],icon:'url(./water.png)'},
+{    name:"Drink Station 2", coordinates:[144.9791479,-37.8564568],icon:'url(./water.png)'},
+{    name:"Drink Station 2A", coordinates:[144.9726751790723,-37.862981793348645],icon:'url(./water.png)'},
+{    name:"Drink Station 3", coordinates:[144.9708854,-37.8513835],icon:'url(./water.png)'},
+{    name:"Drink Station 4", coordinates:[144.9546862,-37.8508159],icon:'url(./water.png)'},
+{    name:"Drink Station 5", coordinates:[144.9775071,-37.8806856],icon:'url(./water.png)'},
+{    name:"Drink Station 6", coordinates:[144.9724424,-37.8255864],icon:'url(./water.png)'},
+{    name:"Drink Station 7", coordinates:[144.9699479,-37.8225651],icon:'url(./water.png)'},
+{    name:"Drink Station 8", coordinates:[144.9751729,-37.8159417],icon:'url(./water.png)'},
+{    name:"Finish", coordinates:[144.98369490614368,-37.81943986264025],icon:'url(./finish.png)'},
+{    name:"42 elite", coordinates:[0,0],icon:'url(./42.png)'},
+{    name:"21 elite", coordinates:[0,0],icon:'url(./21.png)'},
+{    name:"10 first", coordinates:[0,0],icon:'url(./10.png)'},
+{    name:"5 first", coordinates:[0,0],icon:'url(./5.png)'},
+{    name:"2.5 first", coordinates:[0,0],icon:'url(./25.png)'},
+{    name:"Start", coordinates:[144.97620611846554,-37.82089159475012],icon:'url(./start.png)'},
+{    name:"42 last", coordinates:[0,0],icon:'url(./42B.png)'},
+{    name:"42 last deviated", coordinates:[0,0],icon:'url(./42C.png)'},
+{    name:"21 last", coordinates:[0,0],icon:'url(./21B.png)'},
+{    name:"10 last", coordinates:[0,0],icon:'url(./10B.png)'},
+{    name:"5 last", coordinates:[0,0],icon:'url(./5B.png)'},
+{    name:"2.5 last", coordinates:[0,0],icon:'url(./25B.png)'},]
+
+
+
+
+
+
